aa877c1fbdd449ea30f1ebad0c817df6740b293d
Reza <srh.umir@gmail.com>
2016-04-15 13:04:16 +0200

    Two section of the report

--- start.R
+++ start.R
@@ -1,8 +1,16 @@
+set.seed(200)
 exp <- rexp(40000, .2)
+cum <- cumsum(exp)[1:1000]/1:1000
 plot(1:1000, cum, type = "l")
 abline(h=5)
 expmat <- matrix(exp, 1000, 40)
+
 means <- apply(expmat, 1, mean)
+boxplot(means,horizontal = T)
+qplot("Averages",means, geom = "boxplot",horizontal = T)
+
+
+hist(means)
 mean(means)
 vars <- apply(expmat, 1, var)
 varmeans <- var(means)
@@ -16,3 +24,7 @@ lines(x,hx, type = "l", col = "blue", lwd = 2)
 
 hist(rexp(1000, .2))
 hist(means)
+
+round(c(quantile(means,.25),quantile(means,.75)), digits = 2)
+round(quantile(means)[2:3], digits = 2)
+abline(h = 4.436666)
